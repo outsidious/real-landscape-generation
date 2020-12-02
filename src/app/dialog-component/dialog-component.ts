e0ee6c965df3046e2df79e9654e5bbe7658f28c6
Artem <s18b3_vlasenko@179.ru>
2020-12-02 20:08:22 +0300

    base math for painting

--- src/app/dialog-component/dialog-component.ts
+++ src/app/dialog-component/dialog-component.ts
@@ -1,8 +1,10 @@
-import { Component, OnDestroy, Inject, NgZone, OnInit } from '@angular/core';
+import { Component, OnDestroy, Inject, ViewChild, OnInit, ElementRef } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Subscription } from 'rxjs';
 import { BotherService } from '../bother/bother-service';
+import * as math from '../graphics/math';
+import * as rectl from '../graphics/rect';
+import * as caml from '../graphics/camera';
 
 export interface DialogData {
   bounds: string;
@@ -15,6 +17,8 @@ export interface DialogData {
   styleUrls: ['./dialog-component.css'],
 })
 export class DialogComponent implements OnDestroy, OnInit {
+  @ViewChild('canvas') canvas: ElementRef;
+  private ctx: CanvasRenderingContext2D;
   isLoaded: boolean;
   constructor(
     private dialogRef: MatDialogRef<DialogComponent>,
@@ -25,6 +29,11 @@ export class DialogComponent implements OnDestroy, OnInit {
     this.BotherService.runBother(this.data.bounds);
   }
 
+  ngAfterViewInit() {
+    this.ctx = (this.canvas.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.draw();
+  }
+
   loadInfo() {
     this.BotherService.botherSubject.subscribe((data) => {
       this.data.height = data;
@@ -33,11 +42,79 @@ export class DialogComponent implements OnDestroy, OnInit {
     this.isLoaded = false;
   }
 
+  draw() {
+    var width = (this.canvas.nativeElement.width = window.innerWidth - 10);
+    var height = (this.canvas.nativeElement.height = window.innerHeight - 50);
+    let ctx = this.ctx;
+    let p1 = new math.Point3d(300, 300, 0);
+    let p2 = new math.Point3d(500, 300, 0);
+    let p3 = new math.Point3d(500, 500, 0);
+    let p4 = new math.Point3d(300, 500, 0);
+    let rect = new rectl.Rect3d(p1, p2, p3, p4);
+    let scale_coef = new math.Point2d(1, 1);
+    let cam_pos = new math.Point3d(0, 0, 300);
+    let cam_p1 = new math.Point3d(1, 0, 300);
+    let cam_p2 = new math.Point3d(0, 1, 300);
+    let cam_view_p = new math.Point3d(400, 400, 0);
+    let cam = new caml.Camera(cam_pos, cam_p1, cam_p2, cam_view_p);
+    rect.draw(ctx, cam, scale_coef);
+
+    let cameraRotating = false;
+    let x = 0;
+    let y = 0;
+
+    window.onmousedown = function (e) {
+      x = e.offsetX;
+      y = e.offsetY;
+      cameraRotating = true;
+    };
+    window.onmousemove = function (e) {
+      if (cameraRotating) {
+        var xDif = e.offsetX - x;
+        var yDif = e.offsetY - y;
+        cam.rotateAroundZ(xDif * 0.004);
+        cam.rotateAroundX(yDif * 0.003);
+        console.log(cam.position.getDist(cam_view_p));
+        ctx.clearRect(0, 0, width, height);
+        rect.draw(ctx, cam, scale_coef);
+        x =  e.offsetX;
+        y = e.offsetY;
+      }
+    };
+    window.onmouseup = function (e) {
+      cameraRotating = false;
+    };
+
+    window.onmousewheel = function (e: any) {
+      /*
+      if (e.target.id === "slider-'.$question_id.'") {
+        if (!e) {
+          e = window.event;
+        } 
+        if (e.preventDefault) {
+          e.preventDefault();
+        } 
+        e.returnValue = false; 
+      }*/
+      if (e.wheelDelta > 0) {
+        scale_coef.x *= 1.1;
+        scale_coef.y *= 1.1;
+      } else {
+        scale_coef.x /= 1.1;
+        scale_coef.y /= 1.1;
+      }
+      //console.log(scale_coef);
+      ctx.clearRect(0, 0, width, height);
+      rect.draw(ctx, cam, scale_coef);
+    };
+  }
+
   public close() {
     this.dialogRef.close();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   ngOnDestroy() {}
 }
